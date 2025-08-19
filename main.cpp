44c31609d658fa8fdfdc8ee56bcce66e7fb67097
Roy S. <rskulles@gmail.com>
2025-08-19 19:01:42 -0400

    Added comments and some supporting code to calculate frame time.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,20 @@
 
 int main()
 {
+    // initialization
+
     sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 600)), "TEST GAME", sf::Style::None);
+    sf::Clock clock;
+    float delta_time = 0;
+
+    // end initialization
 
     while (window.isOpen())
     {
+        // gets the time between each frame
+        delta_time = clock.reset().asSeconds();
 
+        // handle events
         while (const auto e = window.pollEvent())
         {
             if (e->is<sf::Event::Closed>())
@@ -21,8 +30,16 @@ int main()
                 }
             }
         }
+        // end handle events
+
+        // update logic
+
+        // end update logic
 
         window.clear(sf::Color::Cyan);
+        // rendering code
+
+        // end rendering code
         window.display();
     }
     return 0;
