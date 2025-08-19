faa9e0cd7f411a6b1d2a3d3d0a75d8fe877afda7
Roy S. <rskulles@gmail.com>
2025-08-19 18:44:59 -0400

    Updated window title

--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
-#include<SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 
-int main(){
-    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800,600)),"SKREE SKREE",sf::Style::None);
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800, 600)), "TEST GAME", sf::Style::None);
 
-    while(window.isOpen()){
+    while (window.isOpen())
+    {
 
-        while(const auto e=window.pollEvent()){
-            if(e->is<sf::Event::Closed>()){
+        while (const auto e = window.pollEvent())
+        {
+            if (e->is<sf::Event::Closed>())
+            {
                 window.close();
-            }else if(const auto kp=e->getIf<sf::Event::KeyPressed>()){
-                if(kp->scancode==sf::Keyboard::Scancode::Escape){
+            }
+            else if (const auto kp = e->getIf<sf::Event::KeyPressed>())
+            {
+                if (kp->scancode == sf::Keyboard::Scancode::Escape)
+                {
                     window.close();
                 }
             }
