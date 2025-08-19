176833fa77132be320c105703ecf23deaf5120e3
Roy S. <rskulles@gmail.com>
2025-08-19 16:47:51 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include<SFML/Graphics.hpp>
+
+int main(){
+    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(800,600)),"SKREE SKREE",sf::Style::None);
+
+    while(window.isOpen()){
+
+        while(const auto e=window.pollEvent()){
+            if(e->is<sf::Event::Closed>()){
+                window.close();
+            }else if(const auto kp=e->getIf<sf::Event::KeyPressed>()){
+                if(kp->scancode==sf::Keyboard::Scancode::Escape){
+                    window.close();
+                }
+            }
+        }
+
+        window.clear(sf::Color::Cyan);
+        window.display();
+    }
+    return 0;
+}
